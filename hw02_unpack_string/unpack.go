fd2be6e0d9067391ebc7da22d5dd66122fa598b9
Egor Fomichev <egor.fomichev@ingrammicro.com>
2020-06-08 20:05:45 +0400

    Done with direct mode

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(str string) (string, error) {
+	runes := []rune(str)
+
+	if len(runes) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+
+	var builder strings.Builder
+	var char string
+	for i := 0; i < len(runes)-1; i++ {
+		if unicode.IsDigit(runes[i]) {
+			if unicode.IsDigit(runes[i+1]) {
+				return "", ErrInvalidString
+			}
+			continue
+		}
+
+		char = string(runes[i])
+
+		if unicode.IsDigit(runes[i+1]) {
+
+			countStr := string(runes[i+1])
+			count, _ := strconv.Atoi(countStr)
+
+			if count != 0 {
+				builder.WriteString(strings.Repeat(char, count))
+			}
+		} else {
+			builder.WriteString(char)
+		}
+	}
+
+	indexOfLastRune := len(runes) - 1
+	if unicode.IsDigit(runes[indexOfLastRune]) == false {
+		char = string(runes[indexOfLastRune])
+		builder.WriteString(char)
+	}
+
+	return builder.String(), nil
 }
