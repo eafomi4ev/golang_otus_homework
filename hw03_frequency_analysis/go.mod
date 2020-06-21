5aafe2065ac3735245011e931182d40dc928044d
Egor Fomichev <egor.fomichev@ingrammicro.com>
2020-06-21 17:37:41 +0400

    Home Work 3 done

--- hw03_frequency_analysis/go.mod
+++ hw03_frequency_analysis/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw03_frequency_analysis
+module github.com/eafomi4ev/golang_otus_homework/hw03_frequency_analysis
 
 go 1.14
 
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,61 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const targetWordsCount = 10
+
+type wordFrequency struct {
+	word      string
+	frequency int
+}
+
+func calculateWordsFrequency(words []string) map[string]int {
+	wordsAndFrequency := make(map[string]int)
+
+	for _, word := range words {
+		wordsAndFrequency[word]++
+	}
+
+	return wordsAndFrequency
+}
+
+func splitMapToKeyValueSlice(wordsAndFrequencyMap map[string]int) []wordFrequency {
+	result := make([]wordFrequency, len(wordsAndFrequencyMap))
+
+	var i int
+	for key, value := range wordsAndFrequencyMap {
+		item := wordFrequency{key, value}
+		result[i] = item
+		i++
+	}
+
+	return result
+}
+
+func Top10(initialText string) []string {
+	words := strings.Fields(initialText)
+
+	wordsAndFrequencyMap := calculateWordsFrequency(words)
+	wordFrequencySlice := splitMapToKeyValueSlice(wordsAndFrequencyMap)
+
+	sort.Slice(wordFrequencySlice, func(i, j int) bool {
+		return wordFrequencySlice[i].frequency > wordFrequencySlice[j].frequency
+	})
+
+	var count int
+	if len(wordFrequencySlice) < targetWordsCount {
+		count = len(wordFrequencySlice)
+	} else {
+		count = targetWordsCount
+	}
+
+	mostFrequencyWords := make([]string, count)
+	for i := 0; i < count; i++ {
+		mostFrequencyWords[i] = wordFrequencySlice[i].word
+	}
+
+	return mostFrequencyWords
 }
