6fa61dcd77d0499685dc228b72fcc199c09788dc
Egor Fomichev <eafomichev@avito.ru>
2020-10-22 22:31:20 +0300

    hw05 with errors

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -4,55 +4,106 @@ import (
 	"errors"
 	"fmt"
 	"sync"
-	"time"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-var ErrErrorsJobFailed = errors.New("job failed")
+
+func worker(wg *sync.WaitGroup, doneCh chan struct{}, taskCh chan Job, resultCh chan error, number int) {
+
+	for {
+		select {
+		case <-doneCh:
+			fmt.Printf("Worker %d ОСТАНОВЛЕН\n", number)
+			wg.Done()
+			return
+		case job := <-taskCh:
+			fmt.Printf("Worker %d начал работу над задачей %d\n", number, job.number)
+			result := job.task()
+			fmt.Printf("Worker %d закончил работу над задачей %d\n", number, job.number)
+			resultCh <- result
+		}
+	}
+}
 
 type Task func() error
+
+type Job struct {
+	task   Task
+	number int
+}
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
 	wg := sync.WaitGroup{}
-	taskChanel := make(chan Task)
+	taskCh := make(chan Job)
 	doneCh := make(chan struct{})
-	//errorCount := 0
-	//mu := sync.Mutex{}
-	//resultChanel := make(chan error)
+	resultCh := make(chan error)
+	isResultChClosed := false
+	mu := sync.Mutex{}
 
-	worker := func() {
+	var errorCount int32
+	var tasksDone int32
+
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func() {
+			defer wg.Done()
+
+			for job := range taskCh {
+				if err := job.task(); err != nil {
+					atomic.AddInt32(&errorCount, 1)
+				}
+			}
+		}()
+		//go worker(&wg, doneCh, taskCh, resultCh, i+1)
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer mu.Unlock()
+
 		for {
 			select {
-			case <-doneCh:
-				fmt.Println("Воркер завершен")
-				wg.Done()
-				return
-			case task := <-taskChanel:
-				fmt.Println("Задача взята в работу")
-				task()
+			case ok := <-resultCh:
+				atomic.AddInt32(&tasksDone, 1)
+
+				if ok != nil {
+					atomic.AddInt32(&errorCount, 1)
+					if atomic.LoadInt32(&errorCount) >= int32(M) {
+						mu.Lock()
+						if !isResultChClosed {
+							close(doneCh)
+							isResultChClosed = true
+						}
+						mu.Unlock()
+					}
+				}
 			}
-		}
-	}
-
-	for i := 0; i < N; i++ {
-		wg.Add(1)
-		go worker()
-	}
-
-	for _, task := range tasks {
-		taskChanel <- task
-	}
-	fmt.Println("Все задачи в работе")
-
-	go func() {
-		select {
-		case <-time.After(time.Second * 1):
-			close(doneCh)
 		}
 	}()
 
+	for i, task := range tasks {
+		mu.Lock()
+		if !isResultChClosed {
+			taskCh <- Job{task, i + 1}
+		}
+		mu.Unlock()
+	}
+
+	if !isResultChClosed {
+		close(doneCh)
+		isResultChClosed = true
+	}
+
 	wg.Wait()
+
+	//fmt.Printf("Задач сделано: %d\n", tasksDone)
+
+	if atomic.LoadInt32(&errorCount) >= int32(M) {
+		return ErrErrorsLimitExceeded
+	}
 
 	return nil
 }
