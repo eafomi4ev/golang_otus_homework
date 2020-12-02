7bfd30a29f91b95fa6a5641331a9ae1feadeb1b7
Egor Fomichev <eafomichev@avito.ru>
2020-12-02 19:30:55 +0300

    hw07 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +13,87 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const maxChunkSize = 1 * 1024 * 1024 // 1 Mb -> B
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func evalBytesCountForCopy(size int64, offset int64, limit int64) int64 {
+	bytesCount := size - offset
+	if limit > 0 {
+		bytesCount = min(bytesCount, limit)
+	}
+
+	return bytesCount
+}
+
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	fFrom, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fFrom.Close()
+
+	info, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	fSize := info.Size()
+
+	if offset > fSize {
+		return ErrOffsetExceedsFileSize
+	}
+	if fSize == 0 {
+		return ErrUnsupportedFile
+	}
+
+	chunkSize := min(fSize, maxChunkSize)
+	buff := make([]byte, chunkSize)
+	_, err = fFrom.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	fTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fTo.Close()
+
+	bytesCountForCopy := evalBytesCountForCopy(fSize, offset, limit)
+
+	bar := pb.Full.Start64(bytesCountForCopy)
+	fToBarProxy := bar.NewProxyWriter(fTo)
+
+	var totalReadBytes int64 // сколько всего прочитано байт из источниа
+
+	for {
+		n, err := fFrom.Read(buff)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return err
+		}
+		readBytes := int64(n)
+
+		totalReadBytes += readBytes
+
+		if totalReadBytes >= bytesCountForCopy {
+			_, _ = fToBarProxy.Write(buff[0 : totalReadBytes-(totalReadBytes-bytesCountForCopy)])
+			break
+		} else {
+			_, _ = fToBarProxy.Write(buff)
+		}
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,110 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const inputFileName = "./testdata/input.txt"
+const outputFileName = "./testdata/result.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	t.Cleanup(func() {
+		os.Remove(outputFileName)
+	})
+
+	t.Run("Should return nil if all are ok", func(t *testing.T) {
+		err := Copy(inputFileName, outputFileName, 0, 0)
+
+		require.Nil(t, err)
+	})
+
+	t.Run("offset 0, limit 0", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 0, 0)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset0_limit0.txt")
+
+		require.Equal(t, expectedText, resultText)
+	})
+
+	t.Run("offset 0, limit 10", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 0, 10)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset0_limit10.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("offset 0, limit 1000", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 0, 1000)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset0_limit1000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("offset 0, limit 10000", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 0, 10000)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset0_limit10000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("offset 100, limit 1000", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 100, 1000)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset100_limit1000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("offset 6000, limit 1000", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 6000, 1000)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset6000_limit1000.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("Offset more than file size. Should return error", func(t *testing.T) {
+		err := Copy(inputFileName, outputFileName, 0, 0)
+
+		require.Error(t, ErrOffsetExceedsFileSize, err)
+	})
+
+	t.Run("Limit more than file size. Should copy file up to EOF", func(t *testing.T) {
+		Copy(inputFileName, outputFileName, 0, 9999999999)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset0_limit0.txt")
+
+		require.Equal(t, string(expectedText), string(resultText))
+	})
+
+	t.Run("Limit more than file size and offset isn't 0. Should copy file up to EOF", func(t *testing.T) {
+		offset := int64(6600)
+
+		Copy(inputFileName, outputFileName, offset, 9999999999)
+
+		resultText, _ := ioutil.ReadFile(outputFileName)
+		expectedText, _ := ioutil.ReadFile("./testdata/out_offset0_limit0.txt")
+
+		require.Equal(t, string(expectedText[offset:]), string(resultText))
+	})
+
+	t.Run("File with zero size. Should return error", func(t *testing.T) {
+		err := Copy("/dev/urandom", outputFileName, 0, 0)
+
+		require.Error(t, ErrUnsupportedFile, err)
+	})
 }
